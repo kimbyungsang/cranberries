03e7b384657102f83848eeb0ccfdc2c51c7df257
Egor Suvorov <egor.suvorov@samsung.com>
2017-02-17 15:43:42 +0900

    model-server/zookeeper_cc_util_test: add tests for EnsureTrailingSlash, GetLastPathSegment, all operations (no watchers yet)

--- model-server/zookeeper_cc_util/zookeeper_cc_util_test.cc
+++ model-server/zookeeper_cc_util/zookeeper_cc_util_test.cc
@@ -5,13 +5,50 @@
 #include <iostream>
 #include <sstream>
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
 
 using zookeeper_cc_util::Zookeeper;
+using zookeeper_cc_util::EnsureTrailingSlash;
+using zookeeper_cc_util::GetLastPathSegment;
+using ::testing::UnorderedElementsAre;
+using ::testing::StrEq;
 
 namespace {
 
 const int kRecvTimeoutMs = 1000;
 
+}
+
+TEST(ZookeeperCcUtilEnsureTrailingSlashTest, EmptyString) {
+  EXPECT_EQ("/", EnsureTrailingSlash(""));
+}
+
+TEST(ZookeeperCcUtilEnsureTrailingSlashTest, SingleSlash) {
+  EXPECT_EQ("/", EnsureTrailingSlash("/"));
+}
+
+TEST(ZookeeperCcUtilEnsureTrailingSlashTest, SeveralNodesNoTrailingSlash) {
+  EXPECT_EQ("/node1/node2/", EnsureTrailingSlash("/node1/node2"));
+}
+
+TEST(ZookeeperCcUtilEnsureTrailingSlashTest, SeveralNodesWithTrailingSlash) {
+  EXPECT_EQ("/node1/node2/", EnsureTrailingSlash("/node1/node2/"));
+}
+
+TEST(ZookeeperCcUtilGetLastPathSegmentTest, EmptyString) {
+  EXPECT_THAT(GetLastPathSegment(""), StrEq(""));
+}
+
+TEST(ZookeeperCcUtilGetLastPathSegmentTest, SingleNode) {
+  EXPECT_THAT(GetLastPathSegment("node"), StrEq("node"));
+}
+
+TEST(ZookeeperCcUtilGetLastPathSegmentTest, SingleNodeStartsWithSlash) {
+  EXPECT_THAT(GetLastPathSegment("/node"), StrEq("node"));
+}
+
+TEST(ZookeeperCcUtilGetLastPathSegmentTest, MultipleNodes) {
+  EXPECT_THAT(GetLastPathSegment("/node1/node2"), StrEq("node2"));
 }
 
 class ZookeeperCcUtilTest : public ::testing::Test {
@@ -51,6 +88,19 @@ TEST_F(ZookeeperCcUtilTest, EnforceBasePathAndExists) {
   EXPECT_EQ(ZOK, zk_->Exists("", nullptr, nullptr));
 }
 
+TEST_F(ZookeeperCcUtilTest, EnforceNodeAndExists) {
+  ASSERT_TRUE(zk_->Init());
+  ASSERT_EQ(ZOK, zk_->EnforcePath("some_node", &ZOO_OPEN_ACL_UNSAFE));
+
+  EXPECT_EQ(ZOK, zk_->Exists("some_node", nullptr, nullptr));
+}
+
+TEST_F(ZookeeperCcUtilTest, NodeNotExists) {
+  ASSERT_TRUE(zk_->Init());
+
+  EXPECT_EQ(ZNONODE, zk_->Exists("some_node", nullptr, nullptr));
+}
+
 TEST_F(ZookeeperCcUtilTest, GetCreateGet) {
   ASSERT_TRUE(zk_->Init());
   ASSERT_EQ(ZOK, zk_->EnforcePath("", &ZOO_OPEN_ACL_UNSAFE));
@@ -64,4 +114,33 @@ TEST_F(ZookeeperCcUtilTest, GetCreateGet) {
 
   EXPECT_EQ(ZOK, zk_->Get("some_node", &data, nullptr, nullptr));
   EXPECT_EQ("some_value", data);
+}
+
+TEST_F(ZookeeperCcUtilTest, SetGet) {
+  ASSERT_TRUE(zk_->Init());
+  ASSERT_EQ(ZOK, zk_->EnforcePath("some_node", &ZOO_OPEN_ACL_UNSAFE));
+
+  EXPECT_EQ(ZOK, zk_->Set("some_node", "some_value"));
+
+  std::string data;
+  EXPECT_EQ(ZOK, zk_->Get("some_node", &data, nullptr, nullptr));
+  EXPECT_EQ("some_value", data);
+}
+
+TEST_F(ZookeeperCcUtilTest, GetChildren) {
+  ASSERT_TRUE(zk_->Init());
+  ASSERT_EQ(ZOK, zk_->EnforcePath("node1/1", &ZOO_OPEN_ACL_UNSAFE));
+  ASSERT_EQ(ZOK, zk_->EnforcePath("node1/2", &ZOO_OPEN_ACL_UNSAFE));
+  ASSERT_EQ(ZOK, zk_->EnforcePath("node2/3", &ZOO_OPEN_ACL_UNSAFE));
+  ASSERT_EQ(ZOK, zk_->EnforcePath("node2/4", &ZOO_OPEN_ACL_UNSAFE));
+
+  std::vector<std::string> children;
+  EXPECT_EQ(ZOK, zk_->GetChildren("node1", &children, nullptr));
+  EXPECT_THAT(children, UnorderedElementsAre("1", "2"));
+
+  EXPECT_EQ(ZOK, zk_->GetChildren("node2", &children, nullptr));
+  EXPECT_THAT(children, UnorderedElementsAre("3", "4"));
+
+  EXPECT_EQ(ZOK, zk_->GetChildren("", &children, nullptr));
+  EXPECT_THAT(children, UnorderedElementsAre("node1", "node2"));
 }
